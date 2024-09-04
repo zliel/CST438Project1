97fa63589d1aa09f13ce67f7b386bf5178078c77
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-09-03 17:04:03 -0700

    Added navigation to the App

---
+++ App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {
+    StyleSheet,
+    useColorScheme,
+} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator} from "@react-navigation/stack";
+import Home from "./Screens/Home";
+
+const Stack = createStackNavigator();
+
+const App = () => {
+    const isDarkMode = useColorScheme() === 'dark';
+
+    const backgroundStyle = {
+        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    };
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={Home} />
+                {/*To add a screen, make your component in the "/Screens directory, import it here
+                and add a new Stack.Screen"*/}
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+const styles = StyleSheet.create({
+    sectionContainer: {
+        marginTop: 32,
+        paddingHorizontal: 24,
+    },
+    sectionTitle: {
+        fontSize: 24,
+        fontWeight: '600',
+    },
+    sectionDescription: {
+        marginTop: 8,
+        fontSize: 18,
+        fontWeight: '400',
+    },
+    highlight: {
+        fontWeight: '700',
+    },
+});
+
+export default App;
