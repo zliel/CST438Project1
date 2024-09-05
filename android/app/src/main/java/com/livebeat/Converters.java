3a69ce4155454411ce47a6c32d068fb2c946d1f0
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-09-04 19:28:59 -0700

    Added a note about where I found the TypeConverters information

--- android/app/src/main/java/com/livebeat/Converters.java
+++ android/app/src/main/java/com/livebeat/Converters.java
@@ -6,7 +6,7 @@ import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 
-
+// The solution to the issue of storing arrays was found at https://stackoverflow.com/questions/69792550/how-do-i-set-up-a-room-database-with-an-array-of-data-class
 public class Converters {
     @TypeConverter
     public ArrayList<String> fromString(String value) {
