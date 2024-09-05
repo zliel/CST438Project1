7670c9d9370aeedd59100ac89d83724b8275ccef
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-09-04 18:43:47 -0700

    Added TypeConverters for storing/retrieving ArrayLists for a user's liked_events

---
+++ android/app/src/main/java/com/livebeat/Converters.java
@@ -0,0 +1,21 @@
+package com.livebeat;
+
+import androidx.room.TypeConverter;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+
+public class Converters {
+    @TypeConverter
+    public ArrayList<String> fromString(String value) {
+        Type listType = new TypeToken<ArrayList<String>>() {}.getType();
+        return new Gson().fromJson(value, listType);
+    }
+
+    @TypeConverter
+    public static String fromArrayList(ArrayList<String> arrList) {
+        return new Gson().toJson(arrList);
+    }
+}
