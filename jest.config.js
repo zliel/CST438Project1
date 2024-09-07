ef7b9f389058f690ff050c282224710e8eea6eb7
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-09-06 17:00:47 -0700

    Added settings to the jest config to set up tests for Navigation and handle Module syntax

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   preset: 'react-native',
+  setupFiles: ['./__tests__/jest.setup.js'],
+  transformIgnorePatterns: [
+      'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation)',
+  ],
 };
