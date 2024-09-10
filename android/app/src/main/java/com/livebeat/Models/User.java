3335fa9df63b1955c3435c582c187725c65d46d8
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-09-10 10:08:23 -0700

    Added getters and setters for users

--- android/app/src/main/java/com/livebeat/Models/User.java
+++ android/app/src/main/java/com/livebeat/Models/User.java
@@ -24,4 +24,36 @@ public class User implements Serializable {
         this.password = password;
         this.likedEvents = likedEvents;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public ArrayList<String> getLikedEvents() {
+        return likedEvents;
+    }
+
+    public void setLikedEvents(ArrayList<String> likedEvents) {
+        this.likedEvents = likedEvents;
+    }
 }
