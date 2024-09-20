8cfc1404c4b2c52d1282d257bd2530ce1c35533d
rutujanigade <rnigade@csumb.edu>
2024-09-20 16:15:19 -0700

    Changed from a stack navigator to a drawer navigator

--- App.js
+++ App.js
@@ -1,61 +1,55 @@
 import React from 'react';
-import {
-    StyleSheet,
-    useColorScheme,
-} from 'react-native';
-
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-import {NavigationContainer} from "@react-navigation/native";
-import {createStackNavigator} from "@react-navigation/stack";
-import LandingPage from "./Screens/LandingPage";
+import { StyleSheet, useColorScheme } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
+import LandingPage from './Screens/LandingPage';
 import Login from './Screens/Login';
-import SignUp from './Screens/Signup';
-import ArtistSearch from "./Screens/ArtistSearch";
+import Signup from './Screens/Signup';
+import ArtistSearch from './Screens/ArtistSearch';
+import { ThemeProvider } from './Screens/ThemeContext'; 
 
-const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
 const App = () => {
-    const isDarkMode = useColorScheme() === 'dark';
+  const isDarkMode = useColorScheme() === 'dark';
 
-    const backgroundStyle = {
-        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-    };
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
 
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-
-                {/*To add a screen, make your component in the "/Screens directory, import it here
-                and add a new Stack.Screen"*/}
-                <Stack.Screen name="Landing Page" component={LandingPage}/>
-                  <Stack.Screen name="Login" component={Login} options={{title: ""}}/>
-
-                <Stack.Screen name ="Signup" component={SignUp} options={{title: ""}}/> 
-                {/* <Stack.Screen name="Login" component={Login}/>
-                <Stack.Screen name ="Signup" component={Signup}/> */}
-                <Stack.Screen name="Artist Search" component={ArtistSearch}/>
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
-}
+  return (
+    <ThemeProvider>
+      {/* Wrap only the NavigationContainer, not Drawer.Navigator */}
+      <NavigationContainer>
+        <Drawer.Navigator screenOptions={{unmountOnBlur: true}}>
+          <Drawer.Screen name="Landing Page" component={LandingPage} />
+          <Drawer.Screen name="Login" component={Login} />
+          <Drawer.Screen name="Signup" component={Signup} />
+          <Drawer.Screen name="Artist Search" component={ArtistSearch} />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </ThemeProvider>
+  );
+};
 
 const styles = StyleSheet.create({
-    sectionContainer: {
-        marginTop: 32,
-        paddingHorizontal: 24,
-    },
-    sectionTitle: {
-        fontSize: 24,
-        fontWeight: '600',
-    },
-    sectionDescription: {
-        marginTop: 8,
-        fontSize: 18,
-        fontWeight: '400',
-    },
-    highlight: {
-        fontWeight: '700',
-    },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
 });
 
 export default App;
